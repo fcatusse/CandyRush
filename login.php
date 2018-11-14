70832dd072f231f62ba6e531898bf9ed2f74c312
Camille Clipet <clipet.camille@outlook.fr>
2018-11-14 17:23:01 +0100

    Create login.php again

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+include_once "connect_db.php";
+include_once "login_function.php";
+include_once "config.php";
+session_start();
+
+
+if ($_POST != NULL) {
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+	if (isset($_POST["remember_me"])) {
+		$checked = $_POST["remember_me"];
+	} else {
+		$checked = NULL;
+	}
+	
+	login($email, $password, $checked);
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<form method="post">
+ 		<p>Email: <input type="text" name="email" placeholder="Email"/></p>
+ 		<p>Password: <input type="password" name="password" placeholder="Password"/></p>
+ 		<p><input type="checkbox" name="remember_me"><label> Remember me</label></p>
+ 		<p><input type="submit" value="Submit"></p>
+	</form>
+	<p></p>
+	<p>New user? <a href="signup.php"> Sign up </a></p>
+</html>
