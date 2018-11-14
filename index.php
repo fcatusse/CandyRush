11d83eb367cd44ad0f65f6c1c136c975c84a5e8a
Damian <damian.dugan@coding-academy.fr>
2018-11-14 16:27:43 +0100

    index.php fixed admin interaction hopefully

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+include_once "connect_db.php";
+include_once "login_function.php";
+include_once "config.php";
+include_once "login.php";
 session_start();
 if($_SESSION["name"])
 {
@@ -10,6 +14,7 @@ else
 }
 
 if($_SESSION["is_admin"] == 1 || $_COOKIE["is_admin"] == 1)
+
 
 ?>
 <!DOCTYPE html>
