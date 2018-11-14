e3467cecb6087edc8d27ac88979a44dda503d82a
Camille Clipet <camilleclipet@Camilles-MacBook-Pro.local>
2018-11-14 11:22:48 +0100

    add_user: construct, get, addDB

--- add_user.php
+++ add_user.php
@@ -1,18 +1,36 @@
 <?php
 
-/**
- * 
- */
+include_once "config.php";
+include_once "connect_db.php";
+
 class AddUser
 {
-	private $name;
-	private $email;
-	private $password;
+	private $_username;
+	private $_email;
+	private $_password;
+	private $_isAdmin;
 
-	function __construct($name, $email, $password)
+	function __construct($username, $email, $password, $isAdmin = 0)
 	{
-		# code...
+		$this->_username = $username;
+		$this->_email = $email;
+		$this->_password = $password;
 	}
+
+	function __get ($arg)
+	{
+		return $this->$arg;
+	}
+
+	public function AddToDB ($user)
+	{
+		$port = CONFIG_PORT;
+
+		$pdo = connect_db("localhost", "root", "root", $port, "registration");
+		$query = 'INSERT INTO users (username, password, email, admin) VALUES ( "'.$user->_username.'", "'.$user->_password.'" ,"'.$user->_email.'", "'.$user->_isAdmin.'")';
+		$req->execute();
+	}
+
 }
 
 ?>
