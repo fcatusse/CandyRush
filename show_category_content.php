3e3d16b4b0a71a3a91145037db1414131bf27559
Camille Clipet <clipet.camille@outlook.fr>
2018-11-16 19:27:48 +0100

    add styling

--- show_category_content.php
+++ show_category_content.php
@@ -13,46 +13,94 @@ $prod_list = "";
 $pdo = connect_db("localhost", CONFIG_USER, CONFIG_PASSWORD, CONFIG_PORT, "pool_php_rush");
 
 $category = new CategoryAdmin();
-$array_cat = $category->displayChildrenCategory($_SESSION["category_id"]);
+$product = new ProductAdmin();
+
+$parent_name = $product->getCategoryReverse($_SESSION["category_id"]);
+
+$array_cat = $category->displayChildrenCategory2($_SESSION["category_id"]);
 
 foreach ($array_cat as $value) {
-	$cat_list .= '<p><li> '.$value.' </li></p>';
+	$array = $category->displayCategory($value, "name");
+	$name = $array["name"];
+	//$cat_list .= '<p><li> <a href="show_category_content.php?catprod='.$value.'"> '.$name.' </a></li></p>';
+	$cat_list .= '
+    		<div class="col s12 m5">
+      			<div class="card">
+       				<div class="card-image">
+          				<img src="sampleimg.jpg">
+          				<span class="card-title">'.$name.'</span>
+        			</div>
+        			<div class="card-action">
+          				<a href="show_category_content.php?catprod='.$value.'"> View </a>
+        			</div>
+      			</div>
+    		</div>';
 }
+
+if (isset($_GET["catprod"])) {
+	$id = $_GET["catprod"];
+	$_SESSION["category_id"] = $id;
+	header("Location: show_category_content.php");
+	exit();
+} 
+
+if (isset($_GET["product_id"])) {
+	$id = $_GET["product_id"];
+	$_SESSION["product_id"] = $id;
+	header("Location: show_product.php");
+	exit();
+} 
 
 $array_prod = $category->getChildrenCategories($_SESSION["category_id"]);
 //var_dump($array_prod)."<br>";
-
 foreach ($array_prod as $value) {
-	$query = 'SELECT name FROM products where category_id = '.$value;
+	$query = 'SELECT name, price, id FROM products where category_id = '.$value;
 	$result = $pdo->query($query);
 	$d = $result->fetch(PDO::FETCH_OBJ);
 	//var_dump($d);
 	if ($d != FALSE) {
 		$var = $d->name;
-		$prod_list .= '<p><li> '.$var.' </li></p>';
+		//$prod_list .= '<p><li> <a href="show_product.php?product_id='.$d->id.'"> '.$var.' </li></p>';
+		$prod_list .= '
+    		<div class="col s12 m4">
+      			<div class="card">
+       				<div class="card-image">
+          				<img src="image.jpg">
+          				<span class="card-title">'.$var.'</span>
+        			</div>
+        			<div class="card-content">
+          				<p>'.$d->price.' €</p>
+        			</div>
+        			<div class="card-action">
+          				<a href="show_product.php?product_id='.$d->id.'"> View </a>
+        			</div>
+      			</div>
+    		</div>';
+
 	}
 }
 
 if ($cat_list == "" ) {
-	$cat_list = "<p><li> Nothing to show </li></p>";
+	$cat_list = "<h5><p>Nothing to show!</p></h5>";
 }
 if ($prod_list == "") {
-	$prod_list = "<p><li> Nothing to show </li></p>";
+	$prod_list = "<h5><p>Nothing to show!</p></h5>";
 }
 
 
 
 ?>
 
-<?php include_once "header.php" ; ?>
-	<p> Categories : </p>
-	<ul>
-		<?php echo $cat_list; ?> 
-	</ul>
-    <p> Products: </p>
-    <UL>
-    	<?php echo $prod_list ;?>	
-    </UL>
+<?php include_once "header.php" ;?>
+<div class="container">
+	<h6><a href="#" onClick="window.history.back();">Back</a></h6>
+	<h4> <p> Category <?php echo $parent_name ;?> </p></h>
+	<h5> <p> Categories : </p></h5>
+	<div class="cards row"> <?php echo $cat_list; ?> </div>
+   <h5> <p> Products: </p></h5>
+    <div class="cards row"> <?php echo $prod_list; ?> </div>
+    <h6><a href="#" onClick="window.history.back();">Back</a></h6>
+</div>	
     
-<?php include_once "footer.php" ; ?>
+<?php include_once "footer.php";?>
 
