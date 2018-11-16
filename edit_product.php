e2dd1806b24a6352d09329b00b1d39942845b47d
Camille Clipet <clipet.camille@outlook.fr>
2018-11-16 19:30:56 +0100

    add styling

--- edit_product.php
+++ edit_product.php
@@ -4,6 +4,8 @@ include_once "config.php";
 include_once "user_admin.php";
 include_once "product_admin.php";
 
+$alert = "";
+
 if($_POST!= NULL)
 {
     $is_valid = TRUE;
@@ -22,7 +24,7 @@ if($_POST!= NULL)
 
     if ($is_valid == TRUE) {
         $user = new ProductAdmin();
-        $user->updateProduct($_POST["name"], $_POST["price"], $_POST["candy"], $_SESSION["product_id"]);
+        $alert = $user->updateProduct($_POST["name"], $_POST["price"], $_POST["candy"], $_SESSION["product_id"]);
     }
 }
 $pdo = connect_db("localhost", CONFIG_USER, CONFIG_PASSWORD, CONFIG_PORT, "pool_php_rush");
@@ -37,12 +39,17 @@ while ($d = $result->fetch(PDO::FETCH_OBJ)) {
 ?>
 
 <?php include_once "header.php" ?>
+<div class="container">
+    <h5>Edit a product</h5>
+    <p> <?php echo $alert;?></p>
     <form action="edit_product.php" method="post">
         <p> Name : <input type="text" name="name" required> </p>
          <p> Price : <input type="text" name="price" required> </p>
         <p> Candy category: <select name="candy">
             <?php echo $option ;?>
         </select><p>
-        <p><input type="submit" value="OK"></p>
+        <p><button type="submit" class="waves-effect waves-light btn-small onclick="return confirm('Send the form?')"> OK </button></p>
     </form>
+</div>
+
 <?php include_once "footer.php" ?>
