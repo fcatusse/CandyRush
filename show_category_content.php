87481b2032c8ee93a7ab4c59f0695cbc6ad61603
Camille Clipet <clipet.camille@outlook.fr>
2018-11-16 11:20:15 +0100

    show 1st children categories and all children products

--- show_category_content.php
+++ show_category_content.php
@@ -3,26 +3,52 @@ include_once "connect_db.php";
 include_once "login_function.php";
 include_once "config.php";
 include_once "product_admin.php";
+include_once "category_admin.php";
 session_start();
 
+$cat_list = "";
+$prod_list = "";
+
+//echo $_SESSION["category_id"];
+
 $pdo = connect_db("localhost", CONFIG_USER, CONFIG_PASSWORD, CONFIG_PORT, "pool_php_rush");
-$query = 'SELECT * FROM products WHERE category_id="'.$_SESSION["category_id"].'"';
+
+$category = new CategoryAdmin();
+$array_cat = $category->displayChildrenCategory($_SESSION["category_id"]);
+//var_dump($array_cat);
+
+foreach ($array_cat as $value) {
+	$cat_list .= '<p><li> '.$value.' </li></p>';
+}
+
+$array_prod = $category->getChildrenCategories($_SESSION["category_id"]);
+//var_dump($array_prod)."<br>";
+
+foreach ($array_prod as $value) {
+	$query = 'SELECT name FROM products where category_id = '.$value;
 	$result = $pdo->query($query);
-	while ($d = $result->fetch(PDO::FETCH_OBJ)) {
-	 	$prod_list .= '<p><li> '.$d->name.' '.$d->price.'€ <li></p>';
-    }
-$query2 = 'SELECT * FROM categories WHERE parent_id="'.$_SESSION["category_id"].'"';
-    $result2 = $pdo->query($query2);
-	while ($d = $result2->fetch(PDO::FETCH_OBJ)) {
-	 	$cat_list .= '<p><li> '.$d->name.'</li></p>';
-    }
+	$d = $result->fetch(PDO::FETCH_OBJ);
+	//var_dump($d);
+	if ($d != FALSE) {
+		$var = $d->name;
+		$prod_list .= '<p><li> '.$var.' </li></p>';
+	}
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
-	<?php
-    echo $cat_list ;
-    echo $prod_list ;  
+	<p> Categories : </p>
+	<ul>
+		<?php echo $cat_list; ?> 
+	</ul>
+    <p> Products: </p>
+    <UL>
+    	<?php echo $prod_list ;?>	
+    </UL>
     
-	?>	
 </html>
+
