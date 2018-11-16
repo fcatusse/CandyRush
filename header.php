930bf23de1d56a4e5a7b3cca116813010716a687
Damian <damian.dugan@coding-academy.fr>
2018-11-16 16:24:15 +0100

    header.php implemented nav

--- header.php
+++ header.php
@@ -3,17 +3,22 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <LINK href="/css/materialize_min.css" rel="stylesheet" type="text/css">
-    <script type="text/javascript" src="/css/materialize_min.js"></script>
+    <LINK href="css/materialize.min.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="js/materialize.min.js"></script>
     </head>
     <body>
-    <header>
-        
-        <nav>
+        <header>
         <!-- NAV COMMENCE ICI --->
-
-
-
-
+        
+            <nav>
+            <div class="nav-wrapper">
+                <a href="#!" class="brand-logo center"><img src="lollipop.png">
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                            <li><a href="index.php">Accueil</a></li>
+                            <li><a href="logout.php">Logout</a></li>
+                    </ul>
+                    </div>
+            </nav>
+            
+        </header>
         <!-- NAV TERMINE ICI --->
-        </nav>
