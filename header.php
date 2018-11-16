2e75e9a123a3d47db1528d3e7b0e9a7e024bcd46
fcatusse <45030974+fcatusse@users.noreply.github.com>
2018-11-16 15:46:43 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,19 +1,11 @@
-<?php session_start(); ?>
-
 <!DOCTYPE html>
 <html>
     <head>
-    <LINK href="/css/materialize_min.css" rel="stylesheet" type="text/css">
-    <script type="text/javascript" src="/css/materialize_min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link href="css/materialize.min.css" rel="stylesheet" type="text/css">
+        <script type="text/javascript" src="js/materialize.min.js"></script>
     </head>
     <body>
     <header>
-        
-        <nav>
-        <!-- NAV COMMENCE ICI --->
 
-
-
-
-        <!-- NAV TERMINE ICI --->
-        </nav>
+    </header>
