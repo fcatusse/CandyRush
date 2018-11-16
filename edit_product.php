86bd701ee923c3f9499397247ef60d41c652469e
Camille Clipet <clipet.camille@outlook.fr>
2018-11-16 20:05:19 +0100

    fix missing first category in selector and get current name

--- edit_product.php
+++ edit_product.php
@@ -5,6 +5,7 @@ include_once "user_admin.php";
 include_once "product_admin.php";
 
 $alert = "";
+$option = "";
 
 if($_POST!= NULL)
 {
@@ -27,11 +28,18 @@ if($_POST!= NULL)
         $alert = $user->updateProduct($_POST["name"], $_POST["price"], $_POST["candy"], $_SESSION["product_id"]);
     }
 }
+
+$product = new ProductAdmin();
+$array = $product->displayProduct($_SESSION["product_id"],"name","price", "category_id");
+$name = $array["name"];
+$price = $array["price"];
+$category_id = $array["category_id"];
+$category = $product->getCategoryReverse($category_id); 
+
 $pdo = connect_db("localhost", CONFIG_USER, CONFIG_PASSWORD, CONFIG_PORT, "pool_php_rush");
 $option = NULL;
 $query = 'SELECT * FROM categories';
 $result = $pdo->query($query);
-$d = $result->fetch(PDO::FETCH_OBJ);
 while ($d = $result->fetch(PDO::FETCH_OBJ)) {
    $option .= ' <option value="'.$d->name.'">"'.$d->name.'"</option>';
 }
@@ -43,9 +51,9 @@ while ($d = $result->fetch(PDO::FETCH_OBJ)) {
     <h5>Edit a product</h5>
     <p> <?php echo $alert;?></p>
     <form action="edit_product.php" method="post">
-        <p> Name : <input type="text" name="name" required> </p>
-         <p> Price : <input type="text" name="price" required> </p>
-        <p> Candy category: <select name="candy">
+        <p> Name : <input type="text" name="name" value= <?php echo $name ;?> required> </p>
+         <p> Price : <input type="text" name="price" value= <?php echo $price ;?> required> </p>
+        <p> Candy category (current: <?php echo $category ;?>) <select name="candy" >
             <?php echo $option ;?>
         </select><p>
         <p><button type="submit" class="waves-effect waves-light btn-small onclick="return confirm('Send the form?')"> OK </button></p>
