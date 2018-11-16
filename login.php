216de1e7f7f9bfd9c8d9680cdad021a43c5b3dca
Damian <damian.dugan@coding-academy.fr>
2018-11-16 19:12:52 +0100

    added basic styling to login

--- login.php
+++ login.php
@@ -21,6 +21,46 @@ if ($_POST != NULL) {
 
 <!DOCTYPE html>
 <html>
+	<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <LINK href="css/materialize.min.css" rel="stylesheet" type="text/css">
+    <LINK href="css/bonus.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="utf-8">
+	</head>
+		<div class="container">
+    		<form method="post" class="container">
+		<div class="">
+        <div class="input-field">
+          <input id="email" type="email" class="validate" name="email">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <div class="">
+        <div class="input-field">
+          <input id="password" type="password" class="validate" name="password">
+          <label for="password">Password</label>
+        </div>
+	  </div>
+	  <p>
+      <label>
+        <input type="checkbox" name="remember_me" />
+        <span>Remember me</span>
+      </label>
+	</p>
+	<button class="btn waves-effect waves-light" type="submit" name="action">Submit
+    <i class="material-icons right">send</i>
+  </button>
+
+    </div>
+ 
+  </form>
+  </html>
+        
+<!--<!DOCTYPE html>
+<html>
 	<form method="post">
  		<p>Email: <input type="text" name="email" placeholder="Email"/></p>
  		<p>Password: <input type="password" name="password" placeholder="Password"/></p>
@@ -29,4 +69,4 @@ if ($_POST != NULL) {
 	</form>
 	<p></p>
 	<p>New user? <a href="signup.php"> Sign up </a></p>
-</html>
+</html> --->
